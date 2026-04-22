53606a26ae204c3fb795da7e1f65723735a2d7fc
antoth <antony.poliautre@epitech.eu>
2026-04-22 14:09:47 +0200

    [+] Adding unbreaker need to get it better, but i got collision ball's move and the brick that can be break

--- main.asm
+++ main.asm
@@ -1,10 +1,14 @@
 INCLUDE "hardware.inc"
 
+DEF BRICK_LEFT EQU $05
+DEF BRICK_RIGHT EQU $06
+DEF BLANK_TILE EQU $08
+
 SECTION "Header", ROM0[$100]
 
     jp EntryPoint
 
-    ds $150 - @, 0
+    ds $150 - @
 
 EntryPoint:
 
@@ -14,47 +18,50 @@ WaitVblank:
     jp c, WaitVblank
     ld a, 0
     ld [rLCDC], a
+
+; TitleScreen:
+;     ld de, Unbricked_Title_Screen_Tileset_Begin
+;     ld hl, $9000
+;     ld bc, Unbricked_Title_Screen_Tileset_End - Unbricked_Title_Screen_Tileset_Begin
+;     call MemCpy
+
+;     ld de, Unbricked_Title_Screen_Map_Begin
+;     ld hl, $9800
+;     ld bc, Unbricked_Title_Screen_Map_End - Unbricked_Title_Screen_Map_Begin
+;     call MemCpy
+;     ld a, LCDC_ON | LCDC_BG_ON
+;     ld [rLCDC], a
+;     ld a, %11100100
+;     ld [rBGP], a
+
+; TitleScreenLoop:
+;     call UpdateKeys
+;     ld a, [wCurKeys]
+;     and PAD_START
+;     jr z, TitleScreenLoop
+
+; 	ld a, 0
+; 	ld [rLCDC], a
+	
     ld de, Tiles
     ld hl, $9000
     ld bc, Tiles.End - Tiles
-CopyTiles:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTiles
+	call MemCpy
+
     ld de, Tilemap
     ld hl, $9800
     ld bc, Tilemap.End - Tilemap
-CopyTilemap:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTilemap
-    ld a, LCDC_ON | LCDC_BG_ON
-    ld [rLCDC], a
-    ld a, %11100100
-    ld [rBGP], a
+	call MemCpy
 
-;     ld de, Paddle
-;     ld hl, $8000
-;     ld bc, PaddleEnd - Paddle
-; CoppyPaddle:
-;     ld a, [de]
-;     ld [hli], a
-;     inc de
-;     dec bc
-;     ld a, b
-;     or a, c
-;     jp nz, CoppyPaddle
+    ld de, Paddle
+    ld hl, $8000
+    ld bc, Paddle.End - Paddle
+	call MemCpy
 
-Done:
-    jp Done
+	ld de, Ball
+	ld hl, $8010
+	ld bc, Ball.End - Ball
+	call MemCpy
 
 	ld a, 0
 	ld b, 160
@@ -65,6 +72,8 @@ ClearOam:
 	jp nz, ClearOam
 
 	ld hl, STARTOF(OAM)
+
+	; load the paddle
     ld a, 128 + 16
     ld [hli], a
     ld a, 16 + 8
@@ -73,15 +82,271 @@ ClearOam:
     ld [hli], a
     ld [hli], a
 
-    ld a, LCDC_ON |  LCDC_BG_ON | LCDC_OBJ_ON
-    Ld [rLCDC], A
+	; load the ball
+    ld a, 100 + 16
+    ld [hli], a
+    ld a, 32 + 8
+    ld [hli], a
+    ld a, 1
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
 
+	ld a, 1
+	ld [wBallMomentumX], a
+	ld a, -1
+	ld [wBallMomentumY], a
+
+	; the screen is now on so the objects will be visible as they are updated, but that's fine
+    ld a, LCDC_ON |  LCDC_BG_ON | LCDC_OBJ_ON
+    ld [rLCDC], a
     ld a, %11100100
     ld [rBGP], a
     ld a, %11100100
     ld [rOBP0], a
+	ld a, 0
+	ld [wFrameCounter], a
+	ld [wCurKeys], a
+	ld [wNewKeys], a
 
-SECTION "Tile data", ROM0
+
+
+
+
+
+Main:
+    ld a, [rLY]
+    cp 144
+    jp nc, Main
+WaitVBlank2:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank2
+
+	; pos for the ball, in oam
+	ld a, [wBallMomentumX]
+	ld b, a
+	ld a, [STARTOF(OAM) + 5]
+	add a, b
+	ld [STARTOF(OAM) + 5], a
+	ld a, [wBallMomentumY]
+	ld b, a
+	ld a, [STARTOF(OAM) + 4]
+	add a, b
+	ld [STARTOF(OAM) + 4], a	
+
+    call UpdateKeys
+
+
+Bounce_on_top:
+	ld a, [STARTOF(OAM) + 4]
+	sub a, 16 + 1
+	ld c, a
+	ld a, [STARTOF(OAM) + 5]
+	sub a, 8
+	ld b, a
+	call GetTileByPixel
+	ld a, [hl]
+	call IsWallTile
+	jp nz, BounceOnRight
+	call CheckAndHandleBrick
+	ld a, 1
+	ld [wBallMomentumY], a
+BounceOnRight:
+    ld a, [STARTOF(OAM) + 4]
+    sub a, 16
+    ld c, a
+    ld a, [STARTOF(OAM) + 5]
+    sub a, 8 - 1
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceOnLeft
+	call CheckAndHandleBrick
+    ld a, -1
+    ld [wBallMomentumX], a
+
+BounceOnLeft:
+    ld a, [STARTOF(OAM) + 4]
+    sub a, 16
+    ld c, a
+    ld a, [STARTOF(OAM) + 5]
+    sub a, 8 + 1
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceOnBottom
+	call CheckAndHandleBrick
+    ld a, 1
+    ld [wBallMomentumX], a
+
+BounceOnBottom:
+    ld a, [STARTOF(OAM) + 4]
+    sub a, 16 - 1
+    ld c, a
+    ld a, [STARTOF(OAM) + 5]
+    sub a, 8
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceDone
+	call CheckAndHandleBrick
+    ld a, -1
+    ld [wBallMomentumY], a
+BounceDone:
+    ld a, [STARTOF(OAM)]
+    ld b, a
+    ld a, [STARTOF(OAM) + 4]
+	add a, 4
+    cp a, b
+    jp nz, PaddleBounceDone
+
+
+    ld a, [STARTOF(OAM) + 5]
+    ld b, a
+    ld a, [STARTOF(OAM) + 1]
+    sub a, 8
+    cp a, b
+    jp nc, PaddleBounceDone
+
+
+    add a, 8 + 16
+    cp a, b
+    jp c, PaddleBounceDone
+    ld a, -1
+    ld [wBallMomentumY], a
+
+PaddleBounceDone:
+
+
+CheckLeft:
+    ld a, [wCurKeys]
+    and a, PAD_LEFT
+    jp z, CheckRight
+Left:
+    ld a, [STARTOF(OAM) + 1]
+	;vitesse par frame
+    dec a
+    cp a, 15
+    jp z, Main
+    ld [STARTOF(OAM) + 1], a
+    jp Main
+
+CheckRight:
+    ld a, [wCurKeys]
+    and a, PAD_RIGHT
+    jp z, Main
+Right:
+    ld a, [STARTOF(OAM) + 1]
+	;vitesse par frame
+    inc a
+    cp a, 105
+    jp z, Main
+    ld [STARTOF(OAM) + 1], a
+    jp Main
+
+
+MemCpy:
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	ld a, b
+	or a, c
+	jp nz, MemCpy
+	ret
+
+UpdateKeys:
+  ld a, JOYP_GET_BUTTONS
+  call .onenibble
+  ld b, a
+
+  ld a, JOYP_GET_CTRL_PAD
+  call .onenibble
+  swap a
+  xor a, b
+  ld b, a
+
+  ld a, JOYP_GET_NONE
+  ldh [rJOYP], a
+
+  ld a, [wCurKeys]
+  xor a, b
+  and a, b
+  ld [wNewKeys], a
+  ld a, b
+  ld [wCurKeys], a
+  ret
+
+.onenibble
+  ldh [rJOYP], a
+  call .knownret
+  ldh a, [rJOYP]
+  ldh a, [rJOYP]
+  ldh a, [rJOYP]
+  or a, $F0
+.knownret
+  ret
+
+
+GetTileByPixel:
+ld a, c
+and a, %11111000
+ld l, a
+ld h, 0
+add hl, hl
+add hl, hl
+ld a, b
+srl a
+srl a
+srl a
+add a, l
+ld l, a
+adc a, h
+sub a, l
+ld h, a
+ld bc, $9800
+add hl, bc
+ret
+
+
+CheckAndHandleBrick:
+    ld a, [hl]
+    cp a, BRICK_LEFT
+    jr nz, CheckAndHandleBrickRight
+    ; Break a brick from the left side.
+    ld [hl], BLANK_TILE
+    inc hl
+    ld [hl], BLANK_TILE
+CheckAndHandleBrickRight:
+    cp a, BRICK_RIGHT
+    ret nz
+    ; Break a brick from the right side.
+    ld [hl], BLANK_TILE
+    dec hl
+    ld [hl], BLANK_TILE
+    ret
+
+IsWallTile:
+    cp a, $00
+    ret z
+    cp a, $01
+    ret z
+    cp a, $02
+    ret z
+    cp a, $04
+    ret z
+    cp a, $05
+    ret z
+    cp a, $06
+    ret z
+    cp a, $07
+    ret
+
+
 Tiles:
 	dw `33333333
 	dw `33333333
@@ -173,7 +438,6 @@ Tiles:
 	dw `23232323
 	dw `33333333
 
-	; Paste your logo here!
 	dw `22222222
 	dw `22222222
 	dw `22222222
@@ -319,7 +583,29 @@ Tiles:
 	dw `22222222
 .End:
 
-SECTION "Tilemap", ROM0
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+.End:
+
+Ball:
+    dw `00033000
+    dw `00322300
+    dw `03222230
+    dw `03222230
+    dw `00322300
+    dw `00033000
+    dw `00000000
+    dw `00000000
+.End:
+
+
 Tilemap:
 	db $00, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $02, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 	db $04, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $05, $06, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
@@ -334,21 +620,27 @@ Tilemap:
 	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
-	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $0A, $0B, $0C, $0D, $03, 0,0,0,0,0,0,0,0,0,0,0,0
-	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $0E, $0F, $10, $11, $03, 0,0,0,0,0,0,0,0,0,0,0,0
-	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $12, $13, $14, $15, $03, 0,0,0,0,0,0,0,0,0,0,0,0
-	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $16, $17, $18, $19, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	; db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	; db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	; db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	; db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	; db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+	db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 	db $04, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
+
+
 .End:
 
-; SECTION "Paddle", ROM0
-; Paddle:
-;     dw `13333331
-;     dw `30000003
-;     dw `13333331
-;     dw `00000000
-;     dw `00000000
-;     dw `00000000
-;     dw `00000000
-;     dw `00000000
-; .End:
+SECTION "Counter", WRAM0
+wFrameCounter: db
+
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
+
+SECTION "Ball Data", WRAM0
+wBallMomentumX: db
+wBallMomentumY: db
