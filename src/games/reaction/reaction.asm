100b2533ff207ff4482c2029a95d95063ae5df9e
aryan <aryan.bhotey@epitech.eu>
2026-05-08 17:06:09 +0200

    feat: implement border animation and initialization for title screen

--- src/games/reaction/reaction.asm
+++ src/games/reaction/reaction.asm
@@ -1,26 +1,39 @@
 INCLUDE "hardware.inc"
 SECTION "Reaction Game", ROM0
 
-DEF REACT_STATE_MENU       EQU 0
+;faire des constantes d'état pour avoir une meilleure lisibilité du code
+;plus modulaire pour changer les valeurs facilement
+
+;DEF NOM EQU valeur
+DEF REACT_STATE_MENU EQU 0
 DEF REACT_STATE_DIFFICULTY EQU 1
-DEF REACT_STATE_GAME       EQU 2
-DEF REACT_STATE_WIN        EQU 3
-DEF REACT_STATE_FAIL       EQU 4
+DEF REACT_STATE_GAME EQU 2
+DEF REACT_STATE_WIN EQU 3
+DEF REACT_STATE_FAIL EQU 4
 
 ReactionInit:
-    ld hl, TILEMAP0
-    ld bc, 1024
+    ;clear tilemap ($9800-$9BFF, 1024 octet)
+    ;hl est le registre d'adresse de référence
+    ld hl, TILEMAP0 ;met l'adresse $9800 (début du tilemap en VRAM) dans hl
+    ld bc, 1024 ;bc = 1024, on utilise bc pour les compteurs par conventions
+    xor a ;xor a = ld a, 0 mais pèse 1 octet au lieu de 2
+;on remplit la vram de 0 pour la nettoyer et iniatiliser une variable d'état
+;c un label local qui existera que dans le scope de ReactionInit et ne va pas override dans d'autres labels de d'autres fichiers (c une fct pv)
 .clearTilemap:
     xor a
-    ld [hli], a
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, .clearTilemap
+    ld [hli], a ;ecrit a dans la mémoire pointée par hl et va l'incrémenter ensuite. Donc 1 octet de plus à 0 en VRAM
+    dec bc ;decremente bc dcp bc = 1023. Il ne màj pas le flag z
+    ld a, b ;copie b dans a
+    or a, c ; a = a OR c, ça permet de tester bc == 0. bc = 0 ssi b = 0 et c = 0. il màj le flag z
+    jp nz, .clearTilemap ;si bc != 0 alors en relance
+    ;le premier bloc est un memset(0x9800, 0, 1024)
 
     ld a, REACT_STATE_DIFFICULTY
     ld [wReactionState], a
 
+    ;reset les input
+    ;lors du boot la WRAM contient du garbage puisque la WRAM n'est pas init par le hardware
+    ;init tous les élém de la WRAM pour ne pas boot dans le garbage
     xor a
     ld [wCurKeys], a
     ld [wNewKeys], a
@@ -30,12 +43,16 @@ ReactionInit:
     ld [wWinDrawn], a
     ld [wFailDrawn], a
 
+    ;allumer lcd background
     ld a, LCDC_ON | LCDC_BG_ON
+    ;rLCDC = registre ($FF40) - chaque bit de rLCDC active/desac une fonctionnalité graphique, c'est du PPU
     ld [rLCDC], a
 
 ReactionLoop:
+    ;wait vblank
     call MyWaitVBlank
     call UpdateKeys
+    ;dispatch sur l'état courant
     ld a, [wReactionState]
     cp REACT_STATE_DIFFICULTY
     jp z, DifficultyScreen
@@ -45,7 +62,7 @@ ReactionLoop:
     jp z, WinScreen
     cp REACT_STATE_FAIL
     jp z, FailScreen
-    jp ReactionLoop
+    jp ReactionLoop ;safety net : si état inconnu, on boucle
 
 SECTION "Reaction State", WRAM0
 wReactionState: db
--- src/games/reaction/reaction_title.asm
+++ src/games/reaction/reaction_title.asm
@@ -47,6 +47,97 @@ ReactTitleScreen:
 
     ; cadre : remplir les 16 positions avec TILE_BORDER
     ld c, 0
+.initBorder:
+    ld a, c
+    call GetBorderAddr
+    ld a, TILE_BORDER
+    ld [hl], a
+    inc c
+    ld a, c
+    cp 16
+    jr nz, .initBorder
+
+    xor a
+    ld [wAnimFrame], a
+    ld [wAnimTimer], a
+    call DrawBorderHL
+
+    ld a, LCDC_ON | LCDC_BG_ON
+    ld [rLCDC], a
+    ld a, %11100100
+    ld [rBGP], a
+
+    ; attendre que START soit relâché (évite de catcher le START du menu)
+.waitNotStart:
+    call UpdateKeys
+    ld a, [wCurKeys]
+    and PAD_START
+    jr nz, .waitNotStart
+
+.titleLoop:
+    call MyWaitVBlank
+    call UpdateKeys
+    call UpdateTitleAnim
+    ld a, [wNewKeys]
+    and PAD_START
+    jr z, .titleLoop
+
+    call TransitionScreenToBlack
+    xor a
+    ld [rLCDC], a
+    jp ReactionInit
+
+UpdateTitleAnim:
+    ld a, [wAnimTimer]
+    inc a
+    ld [wAnimTimer], a
+    cp 4
+    ret nz
+    xor a
+    ld [wAnimTimer], a
+
+    ; effacer position courante
+    ld a, [wAnimFrame]
+    call GetBorderAddr
+    ld a, TILE_BORDER
+    ld [hl], a
+
+    ; avancer le frame (0-15)
+    ld a, [wAnimFrame]
+    inc a
+    cp 16
+    jr nz, .noWrap
+    xor a
+.noWrap:
+    ld [wAnimFrame], a
+    call DrawBorderHL
+    ret
+
+DrawBorderHL:
+    ld a, [wAnimFrame]
+    call GetBorderAddr
+    ld a, TILE_BORDER_HL
+    ld [hl], a
+    ret
+
+GetBorderAddr:
+    ; a = index (0-15) → hl = adresse tilemap
+    ld hl, ReactBorderPositions
+    ld b, 0
+    ld c, a
+    add hl, bc
+    add hl, bc ; hl = base + index*2
+    ld a, [hli] ; octet bas (little-endian)
+    ld h, [hl] ; octet haut
+    ld l, a
+    ret
+
+ReactBorderPositions:
+    ; cadre horaire autour de "START" (row 8, cols 8-12)
+    dw $98E7, $98E8, $98E9, $98EA, $98EB, $98EC, $98ED  ; haut (row 7, cols 7-13)
+    dw $990D ; droite (row 8, col 13)
+    dw $992D, $992C, $992B, $992A, $9929, $9928, $9927  ; bas (row 9, cols 13-7)
+    dw $9907 ; gauche (row 8, col 7)
 
 SECTION "React Title Vars", WRAM0
 wAnimFrame: db
