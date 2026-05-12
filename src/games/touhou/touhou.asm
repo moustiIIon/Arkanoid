1dc38e3dd4015be24185202aefc621e0f39e3ae6
aryan <aryan.bhotey@epitech.eu>
2026-05-12 13:33:18 +0200

    add: touhou game init

--- src/games/touhou/touhou.asm
+++ src/games/touhou/touhou.asm
@@ -1,12 +1,46 @@
 INCLUDE "hardware.inc"
 SECTION "Touhou Game", ROM0
 
-DEF TOUHOU_STATE_MENU EQU 0
-DEF TOUHOU_STATE_GAME EQU 1
-DEF TOUHOU_STATE_WIN EQU 2
-DEF TOUHOU_STATE_FAIL EQU 3
+DEF TOUHOU_STATE_GAME EQU 0
+DEF TOUHOU_STATE_OVER EQU 1
 
-Touhou_Init:
-    ld hl, TILEMAP0
-    ld bc, 1024
+TouhouInit:
+    call CommonInit
+
+    ld de, Touhou_Sprite_Begin
+    ld hl, $8000
+    ld bc, Touhou_Sprite_End - Touhou_Sprite_Begin
+    call MemCpy
+
+    ld a, 76
+    ld [wPlayerX], a
+    ld a, 120
+    ld [wPlayerY], a
+
     xor a
+    ld [wTouhouState], a
+    ld [wCurKeys], a
+    ld [wNewKeys], a
+
+    ld a, LCDC_ON | LCDC_BG_ON | LCDC_OBJ_ON | LCDC_OBJ_16
+    ld [rLCDC], a
+    ld a, %11100100
+    ld [rBGP], a
+    ld a, %11100100
+    ld [rOBP0], a
+
+TouhouLoop:
+    call MyWaitVBlank
+    call UpdateKeys
+
+    ld a, [wTouhouState]
+    cp TOUHOU_STATE_GAME
+    jp z, TouhouGameScreen
+    cp TOUHOU_STATE_OVER
+    jp z, TouhouGameOver
+    jp TouhouLoop
+
+SECTION "Touhou Vars", WRAM0
+wTouhouState: db
+wPlayerX: db
+wPlayerY: db
