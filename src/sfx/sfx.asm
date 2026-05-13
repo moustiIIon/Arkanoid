cdc311e356c8e0232b221a0a083f39d4e85cfb21
antoth <antony.poliautre@epitech.eu>
2026-05-13 10:28:58 +0200

    add: new sfx  for brick when those are breaked

--- src/sfx/sfx.asm
+++ src/sfx/sfx.asm
@@ -24,3 +24,16 @@ MusicSelectSfx:
     ld a, $C6
     ldh [rNR14], a
     ret
+
+BrickBreakSfx:
+    ld a, $16
+    ldh [rNR10], a
+    ld a, $80
+    ldh [rNR11], a
+    ld a, $F1
+    ldh [rNR12], a
+    ld a, $D0
+    ldh [rNR13], a
+    ld a, $C4
+    ldh [rNR14], a
+    ret
